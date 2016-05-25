15baf3868116a46d85fc5aca33a52c366883a3f7
TinyZ <ogcs_tinyz@outlook.com>
2016-05-25 17:29:05 +0800

    引入Guest游客概念. 用户由游客->玩家

--- okra-ax-gate/src/main/java/com/lj/kernel/gate/User.java
+++ okra-ax-gate/src/main/java/com/lj/kernel/gate/User.java
@@ -1,25 +1,8 @@
 package com.lj.kernel.gate;
 
-import com.lj.kernel.gpb.generated.GpbRoom;
-import org.ogcs.app.AppContext;
-import org.ogcs.app.Connector;
 import org.ogcs.app.Session;
-import org.ogcs.ax.component.SpringContext;
-import org.ogcs.ax.component.inner.AxInnerClient;
-import org.ogcs.ax.component.inner.AxReplys;
-import org.ogcs.ax.component.manager.AxInnerCoManager;
-import org.ogcs.ax.component.manager.ConnectorManager;
 
-public class User implements Connector {
-
-    private AxInnerCoManager components = (AxInnerCoManager) AppContext.getBean(SpringContext.MANAGER_AX_COMPONENT);
-    private ConnectorManager connectorManager = (ConnectorManager) AppContext.getBean(SpringContext.MANAGER_CONNECTOR);
-
-    private Session session;
-    private Long id;//   uid
-
-    private String module;
-    private long roomId;   //  角色所在房间
+public class User extends Guest {
 
     //    private MemAccount memAccount;
 //    public User(MemAccount memAccount) {
@@ -32,67 +15,13 @@ public class User implements Connector {
 //        this.id = memAccount.getUid();
 //    }
 
-    public long id() {
-        return id;
-    }
 
-    public long getRoomId() {
-        return roomId;
-    }
-
-    public void setRoomId(long roomId) {
-        this.roomId = roomId;
-    }
-
-    @Deprecated
-    public User(Session session) {
-        this.session = session;
-    }
-
-    @Deprecated
     public User(Session session, long uid) {
-        this.session = session;
-        this.id = uid;
+        super(session, uid);
     }
 
 //    public MemAccount getMemAccount() {
 //        return memAccount;
 //    }
 
-    @Override
-    public boolean isConnected() {
-        return session != null && session.isOnline();
-    }
-
-    @Override
-    public Session session() {
-        return session;
-    }
-
-    @Override
-    public void setSession(Session session) {
-        this.session = session;
-    }
-
-    @Override
-    public void disconnect() {
-//        roomManager.exit(this);
-        connectorManager.remove(session);
-
-        if (roomId > 0) {   //  退出房间
-
-            AxInnerClient client = components.getByHash(Modules.module(4), String.valueOf(roomId));
-            if (client != null) {
-                client.push(this.id, 21000,
-                        GpbRoom.ReqExit.newBuilder()
-                                .setRoomId(1)
-                                .setRoomId(roomId)
-                                .build().toByteString()
-                        );
-            }
-            roomId = 0;
-        }
-
-        System.out.println("离线:" + session.toString());
-    }
 }
